0e09e7470fcb89c3003c7462cb9f68a82c19ff54
GnaneshReddy2006 <234g1a0546@srit.ac.in>
2026-02-06 17:13:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
         {/* LEFT */}
         <div className="nav-left">
           <Link to={user ? "/posts" : "/login"} className="logo">
-            Q-Hive
+            ASK-Q
           </Link>
         </div>
 
